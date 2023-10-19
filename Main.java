17ebdd700bd3dc0010811368a2aa93fbab8e0248
MatiasValencia <144842386+MatiasValencia@users.noreply.github.com>
2023-10-19 20:07:35 +0000

    cambio

---
+++ Main.java
@@ -0,0 +1,20 @@
+class Animal {
+    private String nombre;
+    private String color;
+    private int edad;
+    private int extremidades;
+
+    public Animal(){};
+    public Animal(String nombre, String color, int edad, int extremidades) {
+        this.nombre = nombre;
+        this.color = color;
+        this.edad = edad;
+        this.extremidades = extremidades;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
